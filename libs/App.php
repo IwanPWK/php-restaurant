0465e5c50a7382470a54aa8f7e8b05f17ffb6476
IwanPWK <koadhyow@gmail.com>
2023-12-23 22:56:19 +0700

    Selecting data from DB

--- libs/App.php
+++ libs/App.php
@@ -1,36 +1,75 @@
-<?php 
+<?php
 
-require "../config/config.php";	
+require "../config/config.php";
 
-    class App {
+class App
+{
 
-        public $host = HOST;
-        public $dbname = DBNAME;
-        public $user = USER;
-        public $pass = PASS;
+    public $host = HOST;
+    public $dbname = DBNAME;
+    public $user = USER;
+    public $pass = PASS;
 
-        public $link;
+    public $link;
 
+    //create a construct
 
+    public function __construct()
+    {
 
-        //create a construct
+        $this->connect();
+    }
 
-        public function __construct() {
+    public function connect()
+    {
+        $this->link = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->dbname . "", $this->user, $this->pass);
 
-            $this->connect();
+        if ($this->link) {
+            echo "db connection is working";
         }
+    }
 
+    //select all
+    public function selectAll($query)
+    {
 
-        public function connect() {
-            $this->link = new PDO("mysql:host=".$this->host.";dbname=".$this->dbname."", $this->user, $this->pass);
+        $rows = $this->link->query($query);
+        $rows->execute();
 
-            if($this->link) {
-                echo "db connection is working";
-            }
+        $allRows = $rows->fetchAll(PDO::FETCH_OBJ);
+
+        if ($allRows) {
+
+            return $allRows;
+
+        } else {
+
+            return false;
+
         }
+    }
 
+    //select one row
+    public function selectOne($query)
+    {
+
+        $row = $this->link->query($query);
+        $row->execute();
+
+        $singleRow = $row->fetch(PDO::FETCH_OBJ);
+
+        if ($singleRow) {
+
+            return $singleRow;
+
+        } else {
+
+            return false;
+
+        }
 
     }
 
+}
 
-    $obj = new App;
+$obj = new App;
